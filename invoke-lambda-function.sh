3e83710830790734f380ad920eac6695a019adcb
Such-a <giorgi.sachechelashvili.1@btu.edu.ge>
2025-12-20 16:38:37 +0400

    lecture9

---
+++ invoke-lambda-function.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# From code/package-and-deploy directory run ./scripts/invoke-lambda-function.sh <TIME_ZONE>
+
+if [ -z "$1" ]; then
+    echo "Usage: ./scripts/invoke-lambda-function.sh Europe/London"
+    exit 1
+else
+  TIME_ZONE="$1"
+fi
+
+LAMBDA_FUNCTION_NAME="cfn-python-function"
+PAYLOAD="{\"time_zone\":\"${TIME_ZONE}\"}"
+
+aws lambda invoke \
+  --function-name "${LAMBDA_FUNCTION_NAME}" \
+  --payload "${PAYLOAD}" \
+  response.json
+
+cat response.json
